3b458918dd6cc44a104f4ff3e32136874b83599d
Stavros Klmts <stkolome@gmail.com>
2021-07-08 17:00:34 +0300

    added utils file

---
+++ evolved5g/utils.py
@@ -0,0 +1,8 @@
+from cookiecutter.main import cookiecutter
+
+
+def cookiecutter_generate(location, no_input=False, **kwargs):
+    """ Create project from the cookiecutter template in location given
+    with the appropriate arguments. """
+    extra_context = kwargs['extra_context']
+    cookiecutter(location, no_input=no_input, extra_context=extra_context)
