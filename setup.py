8caf4b3b36dfe1a36159a073f538ac2e38d7f105
Stavros Kolometsos <stkolome@gmail.com>
2021-10-19 17:22:03 +0300

    Bump version: 0.2.2 → 0.2.5

--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/EVOLVED-5G/SDK-CLI',
-    version='0.2.2',
+    version='0.2.5',
     zip_safe=False,
 )
