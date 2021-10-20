1cb6d7d01835524b0be239a30ee3bd83eb6871f0
Alexandros Tzoumas <alexandros.tzoumas@gmail.com>
2021-10-20 20:02:05 +0300

    conform to naming standards and  refactoring on tests

--- tests/test_emulator_calls.py
+++ tests/test_emulator_calls.py
@@ -6,37 +6,18 @@ first at url http://localhost:8888
 from evolved5g import swagger_client
 from evolved5g.swagger_client import LoginApi, User
 from evolved5g.swagger_client.models import Token
+from tests import emulator_utils
 
 
 def test_login_to_the_emulator():
-    token = __getToken()
+    token = emulator_utils.get_token()
     assert isinstance(token, Token)
 
 
 def test_token_validation():
-    token = __getToken()
-    configuration = swagger_client.Configuration()
-    configuration.host = __getUrlOfNefEmulator()
-    configuration.access_token = token.access_token
-    api_client = swagger_client.ApiClient(configuration=configuration)
+    token = emulator_utils.get_token()
+    api_client = emulator_utils.get_api_client(token)
     api = LoginApi(api_client)  # noqa: E501
 
     response = api.test_token_api_v1_login_test_token_post_with_http_info()
-    assert isinstance(response, User)
-
-
-def __getToken() -> Token :
-    username = "admin@my-email.com";
-    password = "pass"
-    configuration = swagger_client.Configuration()
-    #The host of the 5G API (emulator)
-    configuration.host = __getUrlOfNefEmulator()
-    api_client = swagger_client.ApiClient(configuration=configuration)
-    api_client.select_header_content_type(["application/x-www-form-urlencoded"])
-    api = LoginApi(api_client)
-    token = api.login_access_token_api_v1_login_access_token_post("", username, password,"" , "", "")
-    return token
-
-def __getUrlOfNefEmulator():
-    return "http://localhost:8888";
-
+    assert isinstance(response[0], User)
--- tests/test_sdk.py
+++
@@ -1,8 +0,0 @@
-"""Tests for `evolved5g` SKD."""
-from evolved5g.sdk import LocationHelper
-
-def test_location_helper_method_a():
-    locationHelper = LocationHelper()
-    response =locationHelper.dummyMethod()
-    assert response ==1
-
