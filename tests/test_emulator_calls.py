44c47e5f9708893f17de9cac7cfc803892ba8dda
Alexandros Tzoumas <alexandros.tzoumas@gmail.com>
2021-10-20 20:46:23 +0300

    skip the tests that depend on the NEF emulator

--- tests/test_emulator_calls.py
+++ tests/test_emulator_calls.py
@@ -3,17 +3,21 @@ Tests that demonstrate how to interact with the Emulator.
 We assume that you have initiated the NEF_EMULATOR https://github.com/EVOLVED-5G/NEF_emulator
 first at url http://localhost:8888
 """
+import pytest
+
 from evolved5g import swagger_client
 from evolved5g.swagger_client import LoginApi, User
 from evolved5g.swagger_client.models import Token
 from tests import emulator_utils
 
 
+@pytest.mark.skip(reason="This can only be run on localhost")
 def test_login_to_the_emulator():
     token = emulator_utils.get_token()
     assert isinstance(token, Token)
 
 
+@pytest.mark.skip(reason="This can only be run on localhost")
 def test_token_validation():
     token = emulator_utils.get_token()
     api_client = emulator_utils.get_api_client(token)
