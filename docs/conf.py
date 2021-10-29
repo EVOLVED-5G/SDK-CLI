dc5458f1634e97e1719c9cb78af588ac09ec5a3b
rimarala <ricardomarco87@gmail.com>
2021-10-29 09:52:33 +0200

    testing documentation v0.4

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ import evolved5g
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'rst2pdf.pdfbuilder']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
