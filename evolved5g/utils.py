08a044c4576e3c31e8dd66a30ff65d93c10b6f20
rimarala <ricardomarco87@gmail.com>
2021-10-29 15:26:52 +0200

    Reverting to the state of the project at commit bbde831b37a925426a8d32a50848c4fec8fe67ad (where pipelines and libraries are working)

--- evolved5g/utils.py
+++ evolved5g/utils.py
@@ -4,5 +4,5 @@ from cookiecutter.main import cookiecutter
 def cookiecutter_generate(location, no_input=False, **kwargs):
     """ Create project from the cookiecutter template in location given
     with the appropriate arguments. """
-    extra_context = kwargs['extra_context']
-    cookiecutter(location, no_input=no_input, extra_context=extra_context)
+    # extra_context = kwargs['extra_context']
+    cookiecutter(location, no_input=no_input) #, extra_context=extra_context)
---
+++ examples/emulator_utils.py
@@ -0,0 +1,31 @@
+from evolved5g import swagger_client
+from evolved5g.swagger_client import LoginApi, User
+from evolved5g.swagger_client.models import Token
+
+
+def get_token() -> Token:
+
+    username = "admin@my-email.com"
+    password = "pass"
+    # User name and pass matches are set in the .env of the docker of NEF_EMULATOR. See
+    # https://github.com/EVOLVED-5G/NEF_emulator
+    configuration = swagger_client.Configuration()
+    # The host of the 5G API (emulator)
+    configuration.host = get_host_of_the_nef_emulator()
+    api_client = swagger_client.ApiClient(configuration=configuration)
+    api_client.select_header_content_type(["application/x-www-form-urlencoded"])
+    api = LoginApi(api_client)
+    token = api.login_access_token_api_v1_login_access_token_post("", username, password, "", "", "")
+    return token
+
+
+def get_api_client(token) -> swagger_client.ApiClient:
+    configuration = swagger_client.Configuration()
+    configuration.host = get_host_of_the_nef_emulator()
+    configuration.access_token = token.access_token
+    api_client = swagger_client.ApiClient(configuration=configuration)
+    return api_client
+
+
+def get_host_of_the_nef_emulator() -> str:
+    return "http://localhost:8888"
