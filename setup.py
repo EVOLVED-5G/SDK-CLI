918ed525f6b2daed5a0808baef526527489a5c9a
a813166 <paula.encinar@atos.net>
2022-01-26 10:07:07 +0100

    Change version to merge pipy

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/EVOLVED-5G/SDK-CLI',
-    version='0.5.6',
+    version='0.6.0',
     zip_safe=False,
 )
