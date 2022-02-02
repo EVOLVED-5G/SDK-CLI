f9abf3e54dc92bfd61fe69ad319f279f9b5b8c49
rimarala <29604142+rimarala@users.noreply.github.com>
2022-02-02 14:40:08 +0100

    Update setup.py
    packages reportlab and pillow removed.

--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/EVOLVED-5G/SDK-CLI',
-    version='0.6.5',
+    version='0.6.6',
     zip_safe=False,
 )
