432ab2825921f29923b3633c8d8e4d3bae60ebf2
Ricardo Marco <ricardo.marco.external@atos.net>
2022-02-03 15:37:49 +0000

    improving documentation v1.5

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,7 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 #html_theme = 'sphinx_rtd_theme'
-html_theme = 'alabaster'
+html_theme = 'classic'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
