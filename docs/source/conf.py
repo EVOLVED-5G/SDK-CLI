4fee4327dd73dd49ad6a4627f7bf43282b2387f7
rimarala <ricardomarco87@gmail.com>
2022-02-17 12:39:00 +0100

    Updating documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,25 +1,10 @@
 # Configuration file for the Sphinx documentation builder.
 
-import os
-import sys
-sys.path.insert(0, os.path.abspath('..'))
-
-#import evolved5g
-
 # -- Project information
 
 project = 'EVOLVED-5G SDK'
 copyright = '2021, EVOLVED-5G'
 author = 'EVOLVED-5G'
-
-# The short X.Y version.
-#version = evolved5g.__version__
-# The full version, including alpha/beta/rc tags.
-#release = evolved5g.__version__
-
-version = "0.1.4"
-release = "0.0.1"
-
 
 # -- General configuration
 
