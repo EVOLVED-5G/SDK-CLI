e5f6d84991cc4cc6d16fe462e4cd7b87d6cdcad2
AlexJoom <alexandros.tzoumas@gmail.com>
2022-10-17 20:13:48 +0300

    capif exposer troubleshooting

--- examples/nef_capif_connector_examples.py
+++ examples/nef_capif_connector_examples.py
@@ -8,7 +8,7 @@ def showcase_capif_nef_connector():
                                             capif_host="capifcore",
                                             capif_http_port="8080",
                                             capif_https_port="443",
-                                            capif_netapp_username="test_nef23",
+                                            capif_netapp_username="test_nef30",
                                             capif_netapp_password="test_netapp_password",
                                             description= "test_app_description"
                                             )
