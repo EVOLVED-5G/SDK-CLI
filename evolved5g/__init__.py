d341df5afae5b484004e2ec45b07690a6f7993a8
AlexJoom <alexandros.tzoumas@gmail.com>
2023-03-01 13:58:39 +0200

    update version

--- evolved5g/__init__.py
+++ evolved5g/__init__.py
@@ -1,7 +1,7 @@
 """Top-level package for Evolved5G_CLI."""
 
 __author__ = "EVOLVED5G project"
-__version__ = "0.9.0"
+__version__ = "1.0.0"
 
 # Uncomment next lines to give direct import access to modules
 # from . import cli
