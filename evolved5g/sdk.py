34fbefdfc91a1476418d68250fa267bf54fc59cc
AlexJoom <alexandros.tzoumas@gmail.com>
2023-07-03 17:58:30 +0300

    DELETE a registration at CAPIF does not return a response

--- evolved5g/sdk.py
+++ evolved5g/sdk.py
@@ -1070,6 +1070,8 @@ class CAPIFInvokerConnector:
         )
         response.raise_for_status()
 
+        response_payload = json.loads(response.text)
+        return response_payload
 
     def __save_capif_ca_root_file_and_get_auth_token(self, role):
 
